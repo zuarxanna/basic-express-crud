a6c788c08f292816b9e740fd36ef5364e650bbe3
zuarxanna <nikhsan417@gmail.com>
2021-12-04 18:23:06 +0700

    add bcrypt

--- controllers/auth/login/index.js
+++ controllers/auth/login/index.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const User = require('../../../models/');
 const generateToken = require('../../../utils/generateToken');
 
@@ -5,7 +6,8 @@ module.exports = async(req, res) => {
     const { username, pass } = await req.body;
     const user = await User.findOne({ username });
     if(!user) return res.status(404).json({ message: "User doesn't exist" })
-    if(user.pass !== pass) return res.status(401).json({ message: `Wrong password for ${username}` });
+    const passValidation = await bcrypt.compare(pass, user.pass);
+    if(!passValidation) return res.status(401).json({ message: `Wrong password for ${username}` });
     const token = await generateToken(user);
     res.header('authorization',token);
     return res.status(200).json({ message: "succesfully logged in", data: { username, pass, token }});
--- controllers/auth/register/index.js
+++ controllers/auth/register/index.js
@@ -1,11 +1,18 @@
 const User = require('../../../models');
+const bcrypt = require('bcrypt');
+
 
 module.exports = async (req, res) => {
-    const usernameUsed = await User.findOne({ username: req.body.username});
+    const usernameUsed = await User.findOne({ username: req.body.username });
     if(usernameUsed) return res.status(409).json("Username already in use");
-    User.insertMany(req.body, (err, user) => {
+    const encryptedPass = await bcrypt.hash(req.body.pass, 10);
+    const newUser = {
+        username: req.body.username,
+        pass: encryptedPass
+    }
+    User.insertMany(newUser, (err, data) => {
         err ? 
         res.status(409).json(err.writeErrors) :
-        res.status(200).json({ message: "User Succesfully Created", data: user});
+        res.status(200).json({ message: "User Succesfully Created", data });
     })
 }
